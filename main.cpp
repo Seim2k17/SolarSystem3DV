3a5d682616396be1a556120a4ce738867eacf6c3
Simon <sng198022@googlemail.com>
2024-08-04 13:36:49 +0200

    003: validationLayerCheck #1 (only debug builds)

--- main.cpp
+++ main.cpp
@@ -4,11 +4,24 @@
 #include <GLFW/glfw3.h>
 
 #include <cstdlib>
+#include <cstring>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
+
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation", "Test1"};
+
+// clang-format off
+#ifdef NDEBUG
+  const bool enableValidationLayers = false;
+#else
+  const bool enableValidationLayers = true;
+#endif
+// clang-format on
 
 class TriangleApp {
 public:
@@ -20,6 +33,32 @@ public:
   }
 
 private:
+  bool checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    // check if all defined validationlayers exists in the availableLayersVector
+    for (const char *layerName : validationLayers) {
+      bool layerFound = false;
+
+      for (const auto &layerProperties : availableLayers) {
+        if (strcmp(layerName, layerProperties.layerName) == 0) {
+          layerFound = true;
+          break;
+        }
+      }
+
+      if (not layerFound) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
   void initWindow() {
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); /// do not a OpenGL context
@@ -48,6 +87,12 @@ private:
   }
 
   void createInstance() {
+    // 0. check for validationLayers (debugging)
+    if (enableValidationLayers && not checkValidationLayerSupport()) {
+      throw std::runtime_error(
+          "validation layers requested, but not available!");
+    }
+
     // 1. set information about application
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
