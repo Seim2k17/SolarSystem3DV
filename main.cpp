3a6a46e394b72cd5761c6009b82606684973c1aa
Simon <sng198022@googlemail.com>
2024-09-02 14:10:44 +0200

    033: integrate ImGui

--- main.cpp
+++ main.cpp
@@ -28,6 +28,10 @@
 #include <vulkan/vk_platform.h>
 #include <vulkan/vulkan_core.h>
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_vulkan.h"
+
 #include <GLFW/glfw3native.h>
 
 #ifdef WIN32
@@ -45,12 +49,28 @@
 #include <glm/glm.hpp> // linear algebra types
 #include <glm/gtc/matrix_transform.hpp>
 
+static void
+check_vk_result(VkResult err)
+{
+    if (err == 0)
+    {
+        return;
+    }
+    fprintf(stderr, "[vulkan] Error: VkResult = %d\n", err);
+
+    if (err < 0)
+    {
+        abort();
+    }
+}
+
 class TriangleApp {
   public:
     void run()
     {
         initWindow();
         initVulkan();
+        // initImGui();
         mainLoop();
         cleanup();
     }
@@ -178,18 +198,245 @@ class TriangleApp {
         createSyncObjects();
     }
 
+    void initImGui()
+    {
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        io.ConfigFlags
+            |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+        io.ConfigFlags
+            |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+        // io.ConfigFlags
+        //    |= ImGuiConfigFlags_DockingEnable; // IF using Docking Branch
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+
+        ImGui_ImplGlfw_InitForVulkan(
+            window, true); // Second param install_callback=true will install
+                           // GLFW callbacks and chain to existing ones.
+
+        ImGui_ImplVulkan_InitInfo init_info = {};
+        init_info.Instance = instance;
+        init_info.PhysicalDevice = physicalDevice;
+        init_info.Device = device;
+        init_info.QueueFamily = graphicsQueueFamily;
+        init_info.Queue = graphicsQueue;
+        init_info.PipelineCache = VK_NULL_HANDLE;
+        init_info.DescriptorPool = descriptorPool;
+        init_info.RenderPass = renderPass;
+        init_info.Subpass = 0;
+        init_info.MinImageCount = 2;
+        init_info.ImageCount = MAX_FRAMES_IN_FLIGHT;
+        init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+        init_info.Allocator = nullptr;
+        init_info.CheckVkResultFn = check_vk_result;
+        ImGui_ImplVulkan_Init(&init_info);
+    }
+
+    VkCommandBuffer BeginSingleTimeCommands(VkDevice device,
+                                            VkCommandPool commandPool)
+    {
+        VkCommandBufferAllocateInfo allocInfo{};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandPool = commandPool;
+        allocInfo.commandBufferCount = 1;
+
+        VkCommandBuffer commandBuffer;
+        vkAllocateCommandBuffers(device, &allocInfo, &commandBuffer);
+
+        VkCommandBufferBeginInfo beginInfo{};
+        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+        beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+        vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+        return commandBuffer;
+    }
+
+    void EndSingleTimeCommands(VkDevice device,
+                               VkCommandPool commandPool,
+                               VkQueue graphicsQueue,
+                               VkCommandBuffer commandBuffer)
+    {
+        vkEndCommandBuffer(commandBuffer);
+
+        VkSubmitInfo submitInfo{};
+        submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+        submitInfo.commandBufferCount = 1;
+        submitInfo.pCommandBuffers = &commandBuffer;
+
+        vkQueueSubmit(graphicsQueue, 1, &submitInfo, VK_NULL_HANDLE);
+        vkQueueWaitIdle(graphicsQueue);
+
+        vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
+    }
+
+    void drawImGui()
+    {
+
+        ImGui_ImplVulkan_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        if (show_demo_window)
+        {
+            ImGui::ShowDemoWindow(); // Show demo window! :)
+        }                            // 3. Show another simple window.
+
+        {
+            ImGuiIO &io = ImGui::GetIO();
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!"); // Create a window called "Hello,
+                                           // world!" and append into it.
+
+            ImGui::Text(
+                "This is some useful text."); // Display some text (you can
+                                              // use a format strings too)
+            ImGui::Checkbox("Demo Window",
+                            &show_demo_window); // Edit bools storing our window
+                                                // open/close state
+            ImGui::Checkbox("Another Window", &show_another_window);
+
+            ImGui::SliderFloat(
+                "float",
+                &f,
+                0.0f,
+                1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::ColorEdit3(
+                "clear color",
+                (float *)&clear_color); // Edit 3 floats representing a color
+
+            if (ImGui::Button(
+                    "Button")) // Buttons return true when clicked (most widgets
+                               // return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                        1000.0f / io.Framerate,
+                        io.Framerate);
+            ImGui::End();
+
+            if (show_another_window)
+            {
+                ImGui::Begin(
+                    "Another Window",
+                    &show_another_window); // Pass a pointer to our bool
+                                           // variable (the window will have a
+                                           // closing button that will clear the
+                                           // bool when clicked)
+                ImGui::Text("Hello from another window!");
+                if (ImGui::Button("Close Me"))
+                    show_another_window = false;
+                ImGui::End();
+            }
+        }
+
+        // last line
+        ImGui::Render();
+    }
+
+    bool show_demo_window = true;
+    bool show_another_window = false;
+    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    VkResult err;
+
     void mainLoop()
     {
+        // Setup Dear ImGui context
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO &io = ImGui::GetIO();
+        io.ConfigFlags
+            |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+        io.ConfigFlags
+            |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+        // io.ConfigFlags
+        //    |= ImGuiConfigFlags_DockingEnable; // IF using Docking Branch
+
+        // Setup Dear ImGui style
+        ImGui::StyleColorsDark();
+
+        ImGui_ImplGlfw_InitForVulkan(
+            window, true); // Second param install_callback=true will install
+                           // GLFW callbacks and chain to existing ones.
+
+        ImGui_ImplVulkan_InitInfo init_info = {};
+        init_info.Instance = instance;
+        init_info.PhysicalDevice = physicalDevice;
+        init_info.Device = device;
+        init_info.QueueFamily = graphicsQueueFamily;
+        init_info.Queue = graphicsQueue;
+        init_info.PipelineCache = VK_NULL_HANDLE;
+        init_info.DescriptorPool = descriptorPool;
+        init_info.RenderPass = renderPass;
+        init_info.Subpass = 0;
+        init_info.MinImageCount = 2;
+        init_info.ImageCount = MAX_FRAMES_IN_FLIGHT;
+        init_info.MSAASamples = VK_SAMPLE_COUNT_1_BIT;
+        init_info.Allocator = nullptr;
+        init_info.CheckVkResultFn = check_vk_result;
+        ImGui_ImplVulkan_Init(&init_info);
+
+        // FIXME: probably the same command buffer like for the normal
+        // renderstuff
+        VkCommandBuffer command_buffer = BeginSingleTimeCommands(
+            device, commandPool); // Helper function to begin command buffer
+        ImGui_ImplVulkan_CreateFontsTexture(); // command_buffer);
+        EndSingleTimeCommands(
+
+            device,
+            commandPool,
+            graphicsQueue,
+            command_buffer); // Helper function to end command buffer
+        // ImGui_ImplVulkan_DestroyFontUploadObjects();
+        ImGui_ImplVulkan_DestroyFontsTexture();
+
         while (not glfwWindowShouldClose(window))
         {
             glfwPollEvents();
+            // drawFrame();
+            // Start the Dear ImGui frame
+            /*if (glfwGetWindowAttrib(window, GLFW_ICONIFIED) != 0)
+            {
+                ImGui_ImplGlfw_Sleep(10);
+                continue;
+            }*/
+            /*
+                    const bool is_minimized = (draw_data->DisplaySize.x <= 0.0f
+               || draw_data->DisplaySize.y <= 0.0f);
+
+                    if (!is_minimized)
+                    {
+                        wd->ClearValue.color.float32[0] = clear_color.x *
+               clear_color.w; wd->ClearValue.color.float32[1] = clear_color.y *
+               clear_color.w; wd->ClearValue.color.float32[2] = clear_color.z *
+               clear_color.w; wd->ClearValue.color.float32[3] = clear_color.w;
+                        FrameRender(wd, draw_data);
+                        FramePresent(wd);
+                    }
+                    */
             drawFrame();
+            // drawImGui(commandBuffer);
+            // advance to the next frame
+            // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         }
 
         // as all operations are async in drawFrame() & when exiting the
         // mainLoop, drawing amy still be going on, cleaning things up while
         // drawing is a bad idea
-        vkDeviceWaitIdle(device);
+        err = vkDeviceWaitIdle(device);
+        check_vk_result(err);
+        ImGui_ImplVulkan_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
     }
 
     /**
@@ -202,6 +449,8 @@ class TriangleApp {
      * */
     void drawFrame()
     {
+        drawImGui();
+
         // synchronization of execution on the GPU is explicit
         // the order of operations is up to us
         // many Vulkan API calls are asychronous
@@ -338,13 +587,14 @@ class TriangleApp {
         {
             throw std::runtime_error("failed to present swap chain image!");
         }
-        // advance to the next frame
+        // advance to next frame here (before ImGui integration)
         currentFrame
             = (currentFrame + 1)
-              % MAX_FRAMES_IN_FLIGHT; /// By using the modulo (%) operator, we
-                                      /// ensure that the frame index loops
-                                      /// around after every
-                                      /// MAX_FRAMES_IN_FLIGHT enqueued frames.
+              % MAX_FRAMES_IN_FLIGHT; /// By using the modulo (%) operator,
+                                      /// we ensure that the frame index
+                                      /// loops around after every
+                                      /// MAX_FRAMES_IN_FLIGHT enqueued
+                                      /// frames.
         // rotateModel(Axis::Y, 90.f, currentFrame);
     }
 
@@ -977,12 +1227,12 @@ class TriangleApp {
             std::cout << "Logical device: " << device << " created."
                       << std::endl;
         }
+        presentQueueFamily = indices.presentFamily.value();
+        graphicsQueueFamily = indices.graphicsFamily.value();
         // as we only create a single queue from this family, we use 0 as a
         // index retrieve the  queue handle
-        vkGetDeviceQueue(
-            device, indices.presentFamily.value(), 0, &presentQueue);
-        vkGetDeviceQueue(
-            device, indices.graphicsFamily.value(), 0, &graphicsQueue);
+        vkGetDeviceQueue(device, presentQueueFamily, 0, &presentQueue);
+        vkGetDeviceQueue(device, graphicsQueueFamily, 0, &graphicsQueue);
     }
 
     void createSwapChain()
@@ -2234,6 +2484,8 @@ class TriangleApp {
         vkCmdDrawIndexed(
             commandBuffer, static_cast<uint32_t>(indices.size()), 1, 0, 0, 0);
 
+        ImGui_ImplVulkan_RenderDrawData(ImGui::GetDrawData(), commandBuffer);
+
         vkCmdEndRenderPass(commandBuffer);
 
         if (vkEndCommandBuffer(commandBuffer) != VK_SUCCESS)
@@ -2816,6 +3068,8 @@ class TriangleApp {
     VkQueue graphicsQueue;
     VkSurfaceKHR surface;
     VkQueue presentQueue;
+    uint32_t presentQueueFamily;
+    uint32_t graphicsQueueFamily;
     VkSwapchainKHR swapChain;
     std::vector<VkImage> swapChainImages;
     VkFormat swapChainImageFormat;
