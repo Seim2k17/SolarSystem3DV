64746e167fcde7889194ddd77f02c3b5f67fba63
Simon Nguyen <sng198022@googlemail.com>
2024-10-20 23:21:44 +0200

    046: introduce vulkan memory allocator
    - for image copy and img memory allocations
    - introduce deletionqueue
    - copy data to imgs instead of directly to swapchain

--- src/engine/sol_types.h
+++ src/engine/sol_types.h
@@ -1,7 +1,12 @@
 #pragma once
 
+#include <vk_mem_alloc.h>
+
 #include <fmt/core.h>
 #include <vulkan/vk_enum_string_helper.h>
+
+#include <queue>
+#include <functional>
 
 #define VK_CHECK(x)                                                     \
     do {                                                                \
@@ -11,3 +16,43 @@
             abort();                                                    \
         }                                                               \
     } while (0)
+
+/*
+** With adding more and more vulkan objects to the engine we need to handle
+** their destruction
+** a common approach is a deletion queue which deletes all the objects in the
+** correct order
+*/
+struct DeletionQueue
+{
+    std::deque<std::function<void()>> deletors;
+
+    void push_function(std::function<void()>&& function) { // store a callback, to be more efficient an array of object handles
+        deletors.push_back(function);
+    }
+
+    void flush() {
+        // reverse iterate the deletion queue to execute all the functions
+        for(auto it= deletors.rbegin();it != deletors.rend(); it++)
+        {
+            (*it)(); // call functors
+        }
+
+        deletors.clear();
+    }
+
+};
+
+
+/*
+** Although drawing the image directly into the swapchain is fine, but come with some restrictions, like low precission.
+** So we render into a separate image. Then we copy that image to the swapchain image and present it to the screen.
+**
+*/
+struct AllocatedImage {
+    VkImage image;
+    VkImageView imageView;
+    VmaAllocation allocation;
+    VkExtent3D imageExtent;
+    VkFormat imageFormat;
+};
